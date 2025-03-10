d5d57a0a74ac316f2f45e3e2589e686176f65fe9
Nick <nikitalytvynov0506@gmail.com>
2025-03-10 14:20:04 +0200

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     formats: ["image/webp"],
+    unoptimized: true,
   },
+  output: "export",
+  basePath: "/interview_task",
+  assetPrefix: "/interview_task",
 };
 
 export default nextConfig;
