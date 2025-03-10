ba60f6f6bd345d015ad9ecfa996780fe7455dddf
Nick <nikitalytvynov0506@gmail.com>
2025-03-10 18:41:22 +0200

    test commit

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // <=== enables static exports
-  reactStrictMode: true,
-  basePath: "/interview_task",
+  output: "export",
+  distDir: "out",
+  images: {
+    unoptimized: true,
+  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
