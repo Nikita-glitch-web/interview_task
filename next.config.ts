98f0c2d0830b23dea4e9477362a3a909dc4b9962
Nick <nikitalytvynov0506@gmail.com>
2025-03-10 18:56:44 +0200

    Last deploy

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ const nextConfig = {
   output: "export",
   distDir: "docs",
   basePath: process.env.NODE_ENV === "production" ? "/interview_task" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/interview_task" : "",
   images: {
     unoptimized: true,
   },
