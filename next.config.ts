5ac1ec11d15a6905d6d2e2672a81b1186f6004a8
Nick <nikitalytvynov0506@gmail.com>
2025-03-10 19:15:09 +0200

    Test deploy

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,8 @@ const nextConfig = {
   output: "export",
   distDir: "docs",
   basePath: process.env.NODE_ENV === "production" ? "/interview_task" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/interview_task" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/interview_task/" : "",
   images: {
     unoptimized: true,
   },
 };
-
-export default nextConfig;
